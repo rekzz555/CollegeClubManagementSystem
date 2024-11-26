777a7966997c971c646b60850c42607607d3c49a
51deep <18689670598@163.com>
2024-11-26 14:40:12 +0800

    Initial commit

---
+++ src/main/java/com/example/bs/BsApplication.java
@@ -0,0 +1,19 @@
+package com.example.bs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsApplication.class, args);
+    }
+
+    //获取完整代码联系QQ86050149 微信deepguan
+    //获取完整代码联系QQ86050149 微信deepguan
+    //获取完整代码联系QQ86050149 微信deepguan
+    //获取完整代码联系QQ86050149 微信deepguan
+    //获取完整代码联系QQ86050149 微信deepguan
+
+}
